ac9b2ea89601ced70382be4c39be6da3ac9e6d18
guisanpea <guisanpea@gmail.com>
2020-01-24 18:56:12 +0100

    Added controller

--- src/main/java/es/uma/ingweb/paas/CorsConfig.java
+++ src/main/java/es/uma/ingweb/paas/CorsConfig.java
@@ -1,21 +1,15 @@
-package es.uma.ingweb.coffeecar;
+package es.uma.ingweb.paas;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                      .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-            }
-        };
+@EnableWebMvc
+public class CorsConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
